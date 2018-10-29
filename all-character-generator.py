80ae14ff743af73e0b066d3d619961e2de6c9c05
Nathu Nandwani <nandwaninathu@yahoo.com>
2018-10-29 17:25:24 +0800

    Added exclusion list

--- all-character-generator.py
+++ all-character-generator.py
@@ -1,7 +1,9 @@
-temp = 'all_chars = ("'
+exclude = ["\x00", "\x01", "\xf8"]
+temp = 'all_chars = (\n"'
 for i in range(0, 256):
     if i % 16 == 0 and i != 0:
         temp += '"' + '\n"'
-    temp += '\\x' + "%0.2X" % i
-temp += '")'
+    if not chr(i) in exclude:
+        temp += '\\x' + "%0.2X" % i
+temp += '"\n)'
 print temp
