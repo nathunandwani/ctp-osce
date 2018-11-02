5746d6884d8d236775e34690943e53cff2b7b577
Nathu Nandwani <nandwaninathu@yahoo.com>
2018-11-02 23:48:16 +0800

    Sub encoder modifications

--- custom-encoder.py
+++ custom-encoder.py
@@ -1,3 +1,4 @@
+#WARNING: VERY SLOPPY CODE AHEAD 
 #NEED TO ADD OPERATORS WITH OTHER REGISTERS
 #Ex: AND EBX, ?
 #    AND ECX, ?
@@ -79,12 +80,79 @@ for k, v in basic_cmds.items():
 
 def sub_encoder(max_limit, shellcode):
     print "SUB Encoder"
-    sub_values = ""
-    for i in shellcode:
-        current = ord(i)
-        counter = 0
-        #if current <= max_limit:
-        #    while 
+    subtract_term1 = []
+    subtract_term2 = []
+    subtract_term3 = []
+    v = tohex(0 - toint(shellcode), 32)
+    v = v.replace("0x", "")
+    v = v.replace("L", "")
+    sh = [v[j:j+2] for j in range(0, len(v), 2)]
+    inner_limit = 0
+    for i in range(0, 256):
+        if not chr(i) in badchars:
+            inner_limit = i
+            break
+    
+    threeterms = False
+    for i in sh:
+        if int(i, 16) > (max_limit - inner_limit) * 2:
+            threeterms = True
+    
+    for i in sh:
+        current = int(i, 16)
+        counter1 = 0
+        counter2 = 0
+        if threeterms:
+            if current > (max_limit - inner_limit) * 2:
+                current -= (max_limit - inner_limit) * 2
+                counter1 = max_limit - inner_limit
+                counter2 = max_limit - inner_limit
+            else:
+                if current > max_limit - inner_limit - inner_limit:
+                    current -= max_limit 
+                    counter1 = max_limit - inner_limit
+                    counter2 = inner_limit
+                else:
+                    current -= inner_limit * 2
+                    counter1 = inner_limit
+                    counter2 = inner_limit
+            while chr(current) in badchars:
+                if current > inner_limit:
+                    current -= 1
+                    if counter1 + 1 <= max_limit and not chr(counter1 + 1) in badchars: 
+                        counter1 += 1
+                    else:
+                        if counter2 + 1 <= max_limit and not chr(counter2 + 1) in badchars:
+                            counter2 += 1
+                        else:
+                            current += 1
+                            print "Couldn't switch values from here (): " + "%0.2X" % current + " | " + "%0.2X" % counter1 + " | " + "%0.2X" % counter2
+                            break
+            subtract_term1.append("%0.2X" % current)
+            subtract_term2.append("%0.2X" % counter1)
+            subtract_term3.append("%0.2X" % counter2)
+        else:
+            current -= 1
+            counter1 += 1
+            while current > (max_limit - 2) or (chr(current) in badchars and current > 0) or chr(counter1) in badchars:
+                current -= 1
+                counter1 += 1
+            subtract_term1.append("%0.2X" % current)
+            subtract_term2.append("%0.2X" % counter1)
+
+    tmpterm1 = ""
+    tmpterm2 = ""
+    tmpterm3 = ""
+    for i in subtract_term1:
+        tmpterm1 += '\\x' + i
+    for i in subtract_term2:
+        tmpterm2 += '\\x' + i
+    for i in subtract_term3:
+        tmpterm3 += '\\x' + i
+    print '"\\x35' + tmpterm1 + '"' + " # SUB EAX, " + ''.join(list(reversed(subtract_term1))) + " # BIN => 35" + ''.join(subtract_term1)
+    print '"\\x35' + tmpterm2 + '"' + " # SUB EAX, " + ''.join(list(reversed(subtract_term2))) + " # BIN => 35" + ''.join(subtract_term2)
+    if threeterms:
+        print '"\\x35' + tmpterm3 + '"' + " # SUB EAX, " + ''.join(list(reversed(subtract_term3))) + " # BIN => 35" + ''.join(subtract_term3)
 
 def operator(operation, fourbytesh):
     if operation == "XOR" and "\x35" in badchars:
@@ -181,13 +249,14 @@ def zero_reg(maximum):
 #    print i
 #print ""
 
-fourbytesh = "\x0f\x42\x52\x6A"
-
+fourbytesh = "\xaf\xea\x75\xaf"
+fourbytesh = "\x2e\xcd\x58\x02"
 print "Original Shellcode: \t" + printshellcode(fourbytesh)
 print ""
 print "Sub Encoder"
 print "Reversed: \t\t" + printshellcode(fourbytesh[::-1])
 print "Two's Complement: \t" + twoscomplement(fourbytesh[::-1])
+sub_encoder(0x7f, fourbytesh[::-1])
 print ""
 zero_reg(1)     
 if operator("XOR", fourbytesh[::-1]) == 0:
